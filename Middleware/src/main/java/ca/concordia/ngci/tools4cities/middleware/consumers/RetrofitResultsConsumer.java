6599dcc944262348b809727e6dd339547b294c95
Gabriel Ullmann <gabriel.cavalheiroullmann@concordia.ca>
2024-10-15 10:25:30 -0300

    remove unused import

--- Middleware/src/main/java/ca/concordia/ngci/tools4cities/middleware/consumers/RetrofitResultsConsumer.java
+++ Middleware/src/main/java/ca/concordia/ngci/tools4cities/middleware/consumers/RetrofitResultsConsumer.java
@@ -3,8 +3,6 @@ package ca.concordia.ngci.tools4cities.middleware.consumers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.Map.Entry;
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
